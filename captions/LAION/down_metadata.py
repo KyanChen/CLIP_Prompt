f2198aa68c4bbe21b41855769a09c7b31713c534
KyanChen <keyan1055002755@163.com>
2022-06-02 11:56:09 +0800

    add

--- captions/LAION/down_metadata.py
+++ captions/LAION/down_metadata.py
@@ -1,8 +1,7 @@
 import os
 
-
+f = open('down_files.txt', 'w')
 for i in range(0, 127):
-    os.system(
-        f'wget https://huggingface.co/datasets/laion/laion2B-en/resolve/main/part-${i:5d}-5114fd87-297e-42b0-9d11-50f1df323dfa-c000.snappy.parquet')
-
-'wget https://huggingface.co/datasets/laion/laion2B-en/resolve/main/part-$00001-5114fd87-297e-42b0-9d11-50f1df323dfa-c000.snappy.parquet'
+    f.write(
+        f'https://huggingface.co/datasets/laion/laion2B-en/resolve/main/part-{i:05d}-5114fd87-297e-42b0-9d11-50f1df323dfa-c000.snappy.parquet\n'
+    )
