af42d91e7e5c6f036271b4bdc54ed0e53cffda47
KyanChen <keyan1055002755@163.com>
2022-06-08 11:08:43 +0800

    add

--- attributes/get_freq.py
+++ attributes/get_freq.py
@@ -17,7 +17,7 @@ def get_key_freq(src_keys, target_data, path, pid):
             try:
                 import pdb
                 pdb.set_trace()
-                tgt_text_list = tgt_text.lower().strip().split(' ')
+                tgt_text_list = tgt_text.lower().strip().strip('.').strip('?').strip('!').strip('\"').split(' ')
                 show_times = []
                 for k in key.split(' '):
                     show_times.append(tgt_text_list.cout(k))
@@ -49,9 +49,9 @@ def gather_all(path, split_num):
 
 if __name__ == '__main__':
     multiprocessing.set_start_method('spawn')
-    n_process = 1
+    n_process = 10
 
-    src_data = json.load(open('../gather_infos/infos/all_attributes.json', 'r'))['attributes']
+    src_data = json.load(open('../gather_infos/infos/all_attributes.json', 'r'))['attributes'][:100]
     data_slice_list = []
     n_item_per_slice = len(src_data) // n_process
     for i in range(n_process):
