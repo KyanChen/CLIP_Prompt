e8576190185fcd648e04ce0c1a06a3efb49c8a04
KyanChen <keyan1055002755@163.com>
2022-06-19 14:24:12 +0800

    add

--- tools/train_script.py
+++ tools/train_script.py
@@ -1,5 +1,5 @@
 import os
 import time
 
-os.system("sh dist_train.sh ../configs_my/maskrcnn_clip_coco.py 4")
+os.system("CUDA_VISIBLE_DEVICES=0,1,2,3 sh dist_train.sh ../configs_my/maskrcnn_clip_coco.py 4")
 
