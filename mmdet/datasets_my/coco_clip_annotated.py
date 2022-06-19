c499d6f6f52282405242283b66bdafa7e87f072f
KyanChen <keyan1055002755@163.com>
2022-06-19 19:26:22 +0800

    add

--- mmdet/datasets_my/coco_clip_annotated.py
+++ mmdet/datasets_my/coco_clip_annotated.py
@@ -51,7 +51,7 @@ class CocoCLIPAnnDataset(CustomDataset):
         for patch_id in self.attributes_dataset['ann_vecs'].keys():
             if self.attributes_dataset['split'][patch_id] == split:
                 self.patch_ids.append(patch_id)
-        self.patch_ids = self.patch_ids[:2]
+        # self.patch_ids = self.patch_ids[:2]
         # list of attribute names
         self.attributes = sorted(
             self.attributes_dataset['attributes'], key=lambda x: x['id'])
