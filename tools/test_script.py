3ea7edd96c0d042db4d1586f8accff9bc6020983
KyanChen <keyan1055002755@163.com>
2022-06-21 00:40:12 +0800

    add

--- tools/test_script.py
+++ tools/test_script.py
@@ -1,10 +1,12 @@
 import os
 import time
 
-os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
-          "sh dist_test.sh "
-          "../configs_my/maskrcnn_clip_coco_ann.py "
-          "results/EXP20220619_1/latest.pth "
-          "8"
-)
+for i in range(20, 120, 20):
+    print('epoch: ', i)
+    os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
+              "sh dist_test.sh "
+              "../configs_my/maskrcnn_clip_coco_ann.py "
+              f"results/EXP20220619_0/epoch_{i}.pth "
+              "8"
+    )
 
