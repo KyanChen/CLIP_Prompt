fe5024272c601a566c456f64f372b40cb6fe7c8b
KyanChen <keyan1055002755@163.com>
2022-06-20 22:07:04 +0800

    add

--- tools/test_script.py
+++ tools/test_script.py
@@ -1,10 +1,10 @@
 import os
 import time
 
-os.system("CUDA_VISIBLE_DEVICES=0,1,2,3 "
+os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
           "sh dist_test.sh "
           "../configs_my/maskrcnn_clip_coco_ann.py "
           "results/EXP20220619_1/latest.pth "
-          "4"
+          "8"
 )
 
