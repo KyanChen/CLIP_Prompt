2b69d9852ed3f609194d7ff0ffb234c15b30afac
KyanChen <keyan1055002755@163.com>
2022-06-28 15:23:47 +0800

    add

--- configs_my/faster_rcnn_r50_fpn_openimages.py
+++ configs_my/faster_rcnn_r50_fpn_openimages.py
@@ -103,7 +103,7 @@ model = dict(
             nms_pre=1000,
             max_per_img=100,
             nms=dict(type='nms', iou_threshold=0.5),
-            min_bbox_size=0),
+            min_bbox_size=16),
         rcnn=dict(
             score_thr=0.01,
             nms=dict(type='nms', iou_threshold=0.5),
