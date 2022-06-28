d1f0471f3911a534dbe4df1f236f9a01c1ce5c5e
KyanChen <keyan1055002755@163.com>
2022-06-28 09:05:25 +0800

    add

--- tools/test_script.py
+++ tools/test_script.py
@@ -1,12 +1,12 @@
 import os
 import time
 
-for i in range(20, 120, 20):
+for i in range(120, 165, 15):
     print('epoch: ', i)
     os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
               "sh dist_test.sh "
-              "../configs_my/maskrcnn_clip_coco_ann.py "
-              f"results/EXP20220619_0/epoch_{i}.pth "
+              "../configs_my/CLIPPrompt_VAW.py "
+              f"results/EXP20220627_0/epoch_{i}.pth "
               "8"
     )
 
