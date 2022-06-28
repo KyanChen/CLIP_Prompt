49761a8b093130e18dec0450f1ac685087726079
KyanChen <keyan1055002755@163.com>
2022-06-28 21:25:53 +0800

    add

--- tools/test_script.py
+++ tools/test_script.py
@@ -5,7 +5,7 @@ for i in range(120, 165, 15):
     print('epoch: ', i)
     os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
               "sh dist_test.sh "
-              "../configs_my/CLIPPrompt_VAW.py "
+              "../configs_my/CLIPPrompt_Proposal_VAW.py "
               f"results/EXP20220627_1/epoch_{i}.pth "
               "8"
     )
