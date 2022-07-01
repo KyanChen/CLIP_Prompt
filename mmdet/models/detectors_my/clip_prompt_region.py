9d13746355b25004f961fc726216afbcd77566f0
KyanChen <keyan1055002755@163.com>
2022-07-01 18:41:39 +0800

    add

--- mmdet/models/detectors_my/clip_prompt_region.py
+++ mmdet/models/detectors_my/clip_prompt_region.py
@@ -121,11 +121,10 @@ class CLIP_Prompter_Region(BaseDetector):
         # torch.Size([256, 512, 28, 28])
         # torch.Size([256, 1024, 14, 14])
         # torch.Size([256, 2048, 7, 7])
+        img_f_maps = tuple([x.float() for x in img_f_maps])
+        img_f_maps = self.neck(img_f_maps)
         import pdb
         pdb.set_trace()
-        img_f_maps = self.neck(img_f_maps)
-
-
         proposal_features, bbox_feats = self.roi_head(img_f_maps, proposals)
 
 
