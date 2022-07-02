9b6fee1acfb8e248955da9eb33d1126914678fe4
KyanChen <keyan1055002755@163.com>
2022-07-02 11:03:19 +0800

    add

--- mmdet/models/necks_my/proposal_encoder.py
+++ mmdet/models/necks_my/proposal_encoder.py
@@ -61,7 +61,8 @@ class ProposalEncoder(BaseModule):
     def forward(self, x, proposal_list, **kwargs):
         rois = bbox2roi(proposal_list)
         bbox_feats = self.bbox_roi_extractor(x[:self.bbox_roi_extractor.num_inputs], rois)  # N 256 7 7
-        bbox_feats = self.shared_head(bbox_feats)
+        if hasattr(self, 'shared_head'):
+            bbox_feats = self.shared_head(bbox_feats)
         proposal_features = self.bbox_head(bbox_feats)
         return proposal_features, bbox_feats
 
