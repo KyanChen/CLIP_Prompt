2770d76dece77ea347190fc2267909480b0229fb
KyanChen <keyan1055002755@163.com>
2022-07-02 10:51:57 +0800

    add

--- tools/train_script.py
+++ tools/train_script.py
@@ -4,6 +4,6 @@ import time
 os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
           "sh dist_train.sh "
           "../configs_my/CLIPPrompt_Region_VAW.py "
-          "results/EXP20220701_0 "
+          "results/EXP20220701_1 "
           "8")
 
