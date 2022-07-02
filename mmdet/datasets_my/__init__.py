bb42603033638fbd000acaa7b1b6626d4d3af91c
KyanChen <keyan1055002755@163.com>
2022-07-02 23:27:56 +0800

    add

--- mmdet/datasets_my/__init__.py
+++ mmdet/datasets_my/__init__.py
@@ -6,6 +6,8 @@ from .coco_clip_annotated import CocoCLIPAnnDataset
 from .vaw_od_datasset import VAWODDataset
 from .vaw_proposal_datasset import VAWProposalDataset
 from .vaw_region_att_pred_datasset import VAWRegionDataset
+from .vaw_crop_img_datasset import VAWCropDataset
 __all__ = [
-    'VAWDataset', 'NWPUDataset', 'CocoCLIPDataset', 'CocoCLIPAnnDataset', 'VAWODDataset', 'VAWProposalDataset', 'VAWRegionDataset'
+    'VAWDataset', 'NWPUDataset', 'CocoCLIPDataset', 'CocoCLIPAnnDataset', 'VAWODDataset', 'VAWProposalDataset',
+    'VAWRegionDataset', 'VAWCropDataset'
 ]
