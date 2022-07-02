eb0b155eac3f173681623cb8dbb56ddbf7bc29c4
KyanChen <keyan1055002755@163.com>
2022-07-02 10:33:03 +0800

    add

--- tools/test_loop.py
+++ tools/test_loop.py
@@ -1,7 +1,7 @@
 import os
 import time
 
-for i_epoch in range(15, 200, 15):
+for i_epoch in range(150, 200, 15):
     os.system(f"python test.py "
               f"--config ../configs_my/CLIPPrompt_Region_VAW.py "
               f"--checkpoint results/EXP20220701_0/epoch_{i_epoch}.pth"
