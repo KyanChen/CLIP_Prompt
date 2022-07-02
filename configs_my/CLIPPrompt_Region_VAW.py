7997747cddc59859f929f297594ec6d2277fd7c3
KyanChen <keyan1055002755@163.com>
2022-07-02 11:13:40 +0800

    add

--- configs_my/CLIPPrompt_Region_VAW.py
+++ configs_my/CLIPPrompt_Region_VAW.py
@@ -140,25 +140,25 @@ data = dict(
     )
 )
 # #
-# # optimizer
-# optimizer = dict(
-#     constructor='SubModelConstructor',
-#     sub_model=['prompt_learner', 'neck', 'roi_head'],
-#     # sub_model=['prompt_learner', 'roi_head'],
-#     type='SGD',
-#     lr=0.005,
-#     momentum=0.9,
-#     weight_decay=0.0005
-# )
-
 # optimizer
 optimizer = dict(
     constructor='SubModelConstructor',
     sub_model=['prompt_learner', 'neck', 'roi_head'],
-    type='Adam',
-    lr=1e-5,
-    weight_decay=1e-3
+    # sub_model=['prompt_learner', 'roi_head'],
+    type='SGD',
+    lr=0.005,
+    momentum=0.9,
+    weight_decay=0.0005
 )
+
+# # optimizer
+# optimizer = dict(
+#     constructor='SubModelConstructor',
+#     sub_model=['prompt_learner', 'neck', 'roi_head'],
+#     type='Adam',
+#     lr=1e-5,
+#     weight_decay=1e-3
+# )
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 
 # # # learning policy
