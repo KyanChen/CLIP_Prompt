1acfa05a562ed7f3548afdf28290d4c59c03fa0e
KyanChen <keyan1055002755@163.com>
2022-07-03 11:19:52 +0800

    add

--- mmdet/models/backbones_my/clip/model.py
+++ mmdet/models/backbones_my/clip/model.py
@@ -162,7 +162,7 @@ class ModifiedResNet(nn.Module):
         x = self.layer4(x)
         outs.append(x)
 
-        # # x_map = None
+        x_map = None
         # x, x_map = self.attnpool(x)
 
         return x, x_map, tuple(outs)
