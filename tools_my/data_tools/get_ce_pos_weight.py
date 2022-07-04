62b792b534f079b5657f99e4d75e9185a4f4e319
KyanChen <keyan1055002755@163.com>
2022-07-04 21:10:38 +0800

    add

--- tools_my/data_tools/get_ce_pos_weight.py
+++ tools_my/data_tools/get_ce_pos_weight.py
@@ -13,15 +13,19 @@ instances = []
 [instances.extend(x) for x in json_data]
 freq_attr = OrderedDict()
 for key, value in attr_dict.items():
-    freq_attr[key] = 0
+    freq_attr[key] = {'pos': 0, 'neg': 0, 'total': 0}
 for instance in tqdm.tqdm(instances):
     positive_attributes = instance['positive_attributes']
     negative_attributes = instance['negative_attributes']
     for item in positive_attributes:
-        freq_attr[item] = freq_attr[item] + 1
+        freq_attr[item]['pos'] = freq_attr[item]['pos'] + 1
+        freq_attr[item]['total'] = freq_attr[item]['total'] + 1
+
     for item in negative_attributes:
-        freq_attr[item] = freq_attr[item] + 1
+        freq_attr[item]['neg'] = freq_attr[item]['neg'] + 1
+        freq_attr[item]['total'] = freq_attr[item]['total'] + 1
+
 json.dump(freq_attr, open(data_root + 'attr_freq_wo_sort.json', 'w'), indent=4)
-freq_attr = sorted(freq_attr.items(), key=lambda kv: kv[1], reverse=True)
+freq_attr = sorted(freq_attr.items(), key=lambda kv: kv[1]['total'], reverse=True)
 json.dump(freq_attr, open(data_root + 'attr_freq_w_sort.json', 'w'), indent=4)
 
