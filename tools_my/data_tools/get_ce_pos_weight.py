ca7242af9cbba93bdeb409d9bd144f5d77203b5b
KyanChen <keyan1055002755@163.com>
2022-07-04 20:57:38 +0800

    add

---
+++ tools_my/data_tools/get_ce_pos_weight.py
@@ -0,0 +1,22 @@
+import json
+from collections import OrderedDict
+
+data_root = '/data1/kyanchen/prompt/data/VAW'
+attr_dict = json.load(open(data_root + 'attribute_index.json'))
+
+
+json_file_list = ["train_part1.json", "train_part2.json"]
+json_data = [json.load(open(data_root + x)) for x in json_file_list]
+instances = []
+[instances.extend(x) for x in json_data]
+freq_attr = OrderedDict()
+for key, value in attr_dict.items():
+    freq_attr[key] = 0
+for instance in instances:
+    positive_attributes = instance['positive_attributes']
+    negative_attributes = instances['negative_attributes']
+    for item in positive_attributes:
+        freq_attr[item] = freq_attr[item] + 1
+    for item in negative_attributes:
+        freq_attr[item] = freq_attr[item] + 1
+json.dump(freq_attr, open(data_root + 'attr_freq.json', 'w'), indent=4)
