5bef7e9ae6be7b32b13a5409de100665aac1e2d7
KyanChen <keyan1055002755@163.com>
2022-07-04 15:51:06 +0800

    add

--- mmdet/models/detectors_my/clip_prompt_region.py
+++ mmdet/models/detectors_my/clip_prompt_region.py
@@ -135,7 +135,7 @@ class CLIP_Prompter_Region(BaseDetector):
         # img_f_maps = tuple([final_map.float()])
 
         # img_f_maps = self.neck(img_f_maps[:len(self.neck.in_channels)])
-        img_f_maps = self.neck(img_f_maps[1:])
+        img_f_maps = self.neck(img_f_maps)
         # torch.Size([28, 256, 224, 224]),
         # torch.Size([28, 256, 112, 112]),
         # torch.Size([28, 256, 56, 56]),
@@ -204,7 +204,7 @@ class CLIP_Prompter_Region(BaseDetector):
         # img_f_maps = tuple([final_map.float()])
 
         # img_f_maps = self.neck(img_f_maps[:len(self.neck.in_channels)])
-        img_f_maps = self.neck(img_f_maps[1:])
+        img_f_maps = self.neck(img_f_maps)
         proposal_features, bbox_feats = self.roi_head(img_f_maps,
                                                       proposals)  # proposal_features: torch.Size([256, 1024, 1, 1])
         proposal_features = rearrange(proposal_features, 'B C H W -> B (C H W)')
