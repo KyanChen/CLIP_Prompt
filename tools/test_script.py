0060e348168cb5f5c5f06e3a03f169e25296f259
chenkeyan <keyan1055002755@163.com>
2022-07-10 16:32:04 +0800

    add

--- tools/test_script.py
+++ tools/test_script.py
@@ -1,12 +1,12 @@
 import os
 import time
 
-for i in range(75, 200, 15):
+for i in range(90, 200, 15):
     print('epoch: ', i)
     os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
               "sh dist_test.sh "
               "../configs_my/CLIPPrompt_Region_VAW.py "
-              f"results/EXP20220702_2/epoch_{i}.pth "
+              f"results/EXP20220701_1/epoch_{i}.pth "
               "8"
     )
 
