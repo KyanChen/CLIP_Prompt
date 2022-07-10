1cf0ff45088af3d0ed0b3d6fe838903fe0de449b
chenkeyan <keyan1055002755@163.com>
2022-07-10 16:04:34 +0800

    add

--- mmdet/datasets_my/vaw_region_att_pred_datasset.py
+++ mmdet/datasets_my/vaw_region_att_pred_datasset.py
@@ -176,7 +176,8 @@ class VAWRegionDataset(Dataset):
         results['proposals'] = proposals
         results['bbox_fields'] = ['proposals']
         results = self.pipeline(results)
-        results['proposals'] = DataContainer(results['proposals'], stack=False)
+        # results['proposals'] = DataContainer(results['proposals'], stack=False)
+        results['proposals'] = results['proposals']
         return results
 
     def __getitem__(self, idx):
