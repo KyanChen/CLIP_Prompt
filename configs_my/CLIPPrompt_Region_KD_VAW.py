4f49bce697de365ca7b21d577c9ded276103f807
chenkeyan <keyan1055002755@163.com>
2022-07-14 12:45:54 +0800

    add

--- configs_my/CLIPPrompt_Region_KD_VAW.py
+++ configs_my/CLIPPrompt_Region_KD_VAW.py
@@ -94,7 +94,7 @@ model = dict(
         re_weight_gamma=2,
         re_weight_beta=0.995,
         balance_unk=0.15,
-        balance_kd=1e4,
+        balance_kd=1e5,
         kd_model_loss=True
     )
 )
--- tools/employ_gpu.py
+++ tools/employ_gpu.py
@@ -1,15 +1,33 @@
 import torch
 import time
+import tqdm
 
 resneXt = torch.hub.load('NVIDIA/DeepLearningExamples:torchhub', 'nvidia_resneXt', pretrained=False)
 
 device = torch.device("cuda:0")
+# device = torch.device("cpu")
 net = torch.nn.DataParallel(resneXt, device_ids=[0, 1, 2, 3, 4, 5, 6, 7])
+# net = resneXt
 net.to(device)
-torch.backends.cudnn.enabled = False
+torch.backends.cudnn.enabled = True
 
 print("Start Running...")
 while True:
     imgs = torch.rand((80, 3, 512, 512)).contiguous()
     imgs = imgs.to(device)
     outputs = net(imgs)
+
+# t0 = time.time()
+# imgs = torch.rand((1, 3, 1024, 1024))
+# for _ in tqdm.tqdm(range(4)):
+#     imgs = imgs.to(device)
+#     outputs = net(imgs)
+# print((time.time() - t0)/4)
+#
+# t0 = time.time()
+# imgs = torch.rand((4, 3, 512, 512))
+# for _ in tqdm.tqdm(range(4)):
+#     imgs = imgs.to(device)
+#     outputs = net(imgs)
+# print((time.time() - t0)/4)
+
