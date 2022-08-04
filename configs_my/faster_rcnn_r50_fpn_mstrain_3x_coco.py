1455ddc3db5a51eafd503efc293dafec14763669
chenkeyan <keyan1055002755@163.com>
2022-08-04 19:01:55 +0800

    add

--- configs_my/faster_rcnn_r50_fpn_mstrain_3x_coco.py
+++ configs_my/faster_rcnn_r50_fpn_mstrain_3x_coco.py
@@ -180,7 +180,7 @@ test_pipeline = [
 
 # Use RepeatDataset to speed up training
 data = dict(
-    samples_per_gpu=24,
+    samples_per_gpu=16,
     workers_per_gpu=2,
     train=dict(
         type='RepeatDataset',
