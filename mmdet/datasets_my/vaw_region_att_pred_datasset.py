be5a7666f27cba5a7b887e6fee8772cab833ef86
chenkeyan <keyan1055002755@163.com>
2022-08-04 17:59:36 +0800

    add

--- mmdet/datasets_my/vaw_region_att_pred_datasset.py
+++ mmdet/datasets_my/vaw_region_att_pred_datasset.py
@@ -217,6 +217,8 @@ class VAWRegionDataset(Dataset):
         results = self.pipeline(results)
         # results['proposals'] = DataContainer(results['proposals'], stack=False)
         # results['proposals'] = results['proposals']
+        import pdb
+        pdb.set_trace()
         results['img'] = DataContainer(results['img'], padding_value=0, stack=True)
         return results
 
