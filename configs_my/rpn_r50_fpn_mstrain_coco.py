c23d49f8bea4779d2ac65804ac94035cf5f3e58f
chenkeyan <keyan1055002755@163.com>
2022-08-08 17:13:14 +0800

    add

--- configs_my/rpn_r50_fpn_mstrain_coco.py
+++ configs_my/rpn_r50_fpn_mstrain_coco.py
@@ -176,7 +176,7 @@ data_root = '/data/kyanchen/prompt/data/COCO'
 
 # dataset_type = 'VAWODDataset'
 # data_root = '/data/kyanchen/prompt/data'
-samples_per_gpu = 32
+samples_per_gpu = 30
 data = dict(
     samples_per_gpu=samples_per_gpu,
     workers_per_gpu=4,
