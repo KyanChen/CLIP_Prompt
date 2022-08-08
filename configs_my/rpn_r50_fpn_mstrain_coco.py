875e23c8c07e38d5ce95e15890c283cd2a66030e
chenkeyan <keyan1055002755@163.com>
2022-08-08 17:15:08 +0800

    add

--- configs_my/rpn_r50_fpn_mstrain_coco.py
+++ configs_my/rpn_r50_fpn_mstrain_coco.py
@@ -38,7 +38,7 @@ model = dict(
         norm_cfg=dict(type='BN', requires_grad=True),
         norm_eval=True,
         style='pytorch',
-        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
+        init_cfg=dict(type='Pretrained', map_location='cpu', checkpoint='torchvision://resnet50')),
     # backbone=dict(
     #     type='CLIPModel',
     #     backbone_name='RN50',
