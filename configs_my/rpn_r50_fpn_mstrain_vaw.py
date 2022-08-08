53372a456fd47e61f0c7f0e9893bb952f279fb05
chenkeyan <keyan1055002755@163.com>
2022-08-08 10:12:28 +0800

    add

--- configs_my/rpn_r50_fpn_mstrain_vaw.py
+++ configs_my/rpn_r50_fpn_mstrain_vaw.py
@@ -26,16 +26,25 @@ auto_scale_lr = dict(enable=False, base_batch_size=16)
 
 model = dict(
     type='FasterRCNNRPN',
+    # backbone=dict(
+    #     type='ResNet',
+    #     depth=50,
+    #     num_stages=4,
+    #     out_indices=(0, 1, 2, 3),
+    #     frozen_stages=1,
+    #     norm_cfg=dict(type='BN', requires_grad=True),
+    #     norm_eval=True,
+    #     style='pytorch',
+    #     init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
     backbone=dict(
-        type='ResNet',
-        depth=50,
-        num_stages=4,
-        out_indices=(0, 1, 2, 3),
-        frozen_stages=1,
-        norm_cfg=dict(type='BN', requires_grad=True),
-        norm_eval=True,
-        style='pytorch',
-        init_cfg=dict(type='Pretrained', checkpoint='torchvision://resnet50')),
+        type='CLIPModel',
+        backbone_name='RN50',
+        with_attn=False,
+        out_indices=[1, 2, 3, 4],
+        # backbone_name='ViT-B/16',
+        load_ckpt_from=None,
+        precision='fp32',
+    ),
     neck=dict(
         type='FPN',
         in_channels=[256, 512, 1024, 2048],
@@ -44,7 +53,7 @@ model = dict(
     rpn_head=dict(
         type='RPNHead',
         in_channels=256,
-        num_convs=1,
+        num_convs=3,
         feat_channels=256,
         anchor_generator=dict(
             type='AnchorGenerator',
