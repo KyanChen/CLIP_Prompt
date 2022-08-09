f20146a124b43429aae585f40925170f574831cd
chenkeyan <keyan1055002755@163.com>
2022-08-09 16:36:16 +0800

    add

--- tools/train_script.py
+++ tools/train_script.py
@@ -11,8 +11,9 @@ while True:
               # "../configs_my/faster_rcnn_r50_fpn_mstrain_3x_coco.py "
               # "../configs_my/rpn_r50_fpn_mstrain_coco.py "
               # "../configs_my/rpn_r50_fpn_mstrain_vaw.py "
-              "../configs_my/CLIPPrompt_Region_FasterRcnn_KD_VAW.py "
-              "results/EXP20220809_2 "
+              "../configs_my/rpn_r50_fpn_mstrain_vg.py "
+              # "../configs_my/CLIPPrompt_Region_FasterRcnn_KD_VAW.py "
+              "results/EXP20220809_3 "
               "8")
     time.sleep(60*2)
 
