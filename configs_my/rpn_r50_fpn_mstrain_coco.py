e7a19caedf6a0f79592161a9e1e8064c8841511f
chenkeyan <keyan1055002755@163.com>
2022-08-09 16:58:39 +0800

    add

--- configs_my/rpn_r50_fpn_mstrain_coco.py
+++ configs_my/rpn_r50_fpn_mstrain_coco.py
@@ -176,7 +176,7 @@ data_root = '/data1/kyanchen/DetFramework/data/COCO/'
 
 # dataset_type = 'VAWODDataset'
 # data_root = '/data/kyanchen/prompt/data'
-# find_unused_parameters = True
+find_unused_parameters = True
 samples_per_gpu = 20
 data = dict(
     samples_per_gpu=samples_per_gpu,
