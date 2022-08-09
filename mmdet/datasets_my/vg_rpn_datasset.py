af19d43b34bce98ffcf6b37c9add882621a08022
chenkeyan <keyan1055002755@163.com>
2022-08-09 16:29:40 +0800

    add

--- mmdet/datasets_my/vg_rpn_datasset.py
+++ mmdet/datasets_my/vg_rpn_datasset.py
@@ -105,8 +105,13 @@ class VGRPNDataset(Dataset):
             img_instances_pair_all[data['image_id']] = data['objects']
         img_instances_pair = {}
         for img_id in img_ids:
-            if len(img_instances_pair_all[img_id]) > 0:
-                img_instances_pair[img_id] = img_instances_pair_all[img_id]
+            instances = []
+            for instance in img_instances_pair_all[img_id]:
+                x, y, w, h = instance["x"], instance["y"], instance["w"], instance["h"]
+                if w * h > 0:
+                    instances.append(instance)
+            if len(instances):
+                img_instances_pair[img_id] = instances
         return img_instances_pair
 
     def __len__(self):
