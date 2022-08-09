b71db28612d68b923a9aaafac440f40e6aabdad2
chenkeyan <keyan1055002755@163.com>
2022-08-09 08:52:34 +0800

    add

--- configs_my/rpn_r50_fpn_mstrain_vaw.py
+++ configs_my/rpn_r50_fpn_mstrain_vaw.py
@@ -173,8 +173,9 @@ test_pipeline = [
 dataset_type = 'VAWRPNDataset'
 
 # data_root = '/data1/kyanchen/DetFramework/data/COCO/'
-data_root = '/data1/kyanchen/prompt/data'
+# data_root = '/data1/kyanchen/prompt/data'
 # data_root = '/data/kyanchen/Data'
+data_root = '/data/kyanchen/prompt/data'
 
 samples_per_gpu = 20
 data = dict(
