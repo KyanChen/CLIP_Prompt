e752503c4510c1064e1c98ec865d76c36d161d38
chenkeyan <keyan1055002755@163.com>
2022-08-10 10:04:10 +0800

    add

--- tools/test_script.py
+++ tools/test_script.py
@@ -5,8 +5,8 @@ import time
 os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
           "sh dist_test.sh "
           # "../configs_my/rpn_r50_fpn_mstrain_vaw.py "
-          "../configs_my/rpn_r50_fpn_mstrain_vg.py "
-          f"results/EXP20220807_0/latest.pth "
+          "../configs_my/rpn_r50_fpn_mstrain_coco.py "
+          f"results/EXP20220809_4/latest.pth "
           "8"
 )
 
