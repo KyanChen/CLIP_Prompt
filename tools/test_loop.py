727a1bf5dfa68c6491bcabfc35ff6f37d0c4a6ca
KyanChen <keyan1055002755@163.com>
2022-08-17 18:46:05 +0800

    add

--- tools/test_loop.py
+++ tools/test_loop.py
@@ -1,7 +1,7 @@
 import os
 import time
 
-for i_epoch in range(150, 200, 15):
+for i_epoch in range(40, 65, 5):
     os.system(f"python test.py "
               f"--config ../configs_my/CLIPPrompt_Region_VAW.py "
               f"--checkpoint results/EXP20220807_0/epoch_{i_epoch}.pth"
--- tools/test_script.py
+++ tools/test_script.py
@@ -4,10 +4,11 @@ import time
 
 os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
           "sh dist_test.sh "
-          "../configs_my/RPN_CLIPPrompt_Region_KD_COCO_VAW.py "
+          # "../configs_my/RPN_CLIPPrompt_Region_KD_COCO_VAW.py "
           # '../configs_my/rpn_r50_fpn_mstrain_vg.py '
+          '../configs_my/rpn_r50_fpn_mstrain_vaw.py '
           # "../configs_my/rpn_r50_fpn_mstrain_coco.py "
-          f"results/EXP20220814_1/latest.pth "
+          f"results/EXP20220814_0/epoch_40.pth "
           "8"
 )
 
