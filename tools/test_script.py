63d9192ea26ce43a56febe4081c8bdf99c44525a
KyanChen <keyan1055002755@163.com>
2022-08-19 14:39:27 +0800

    add

--- tools/test_script.py
+++ tools/test_script.py
@@ -8,7 +8,7 @@ os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
           # '../configs_my/rpn_r50_fpn_mstrain_vg.py '
           # '../configs_my/rpn_r50_fpn_mstrain_vaw.py '
           # "../configs_my/rpn_r50_fpn_mstrain_coco.py "
-          f"results/EXP20220814_1/epoch_55.pth "
+          f"results/EXP20220817_0/epoch_50.pth "
           "8"
 )
 
