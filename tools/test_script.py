2772fc2d5001517c918e2e5d1a0632f38a6a25f9
KyanChen <keyan1055002755@163.com>
2022-08-24 14:38:57 +0800

    add

--- tools/test_script.py
+++ tools/test_script.py
@@ -4,11 +4,12 @@ import time
 
 os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
           "sh dist_test.sh "
-          "../configs_my/RPN_CLIPPrompt_Region_KD_COCO_VAW.py "
+          # "../configs_my/RPN_CLIPPrompt_Region_KD_COCO_VAW.py "
           # '../configs_my/rpn_r50_fpn_mstrain_vg.py '
           # '../configs_my/rpn_r50_fpn_mstrain_vaw.py '
           # "../configs_my/rpn_r50_fpn_mstrain_coco.py "
-          f"results/EXP20220818_0/epoch_40.pth "
+          "../configs_my/CLIPPrompt_Crop_Img_VAW.py "
+          f"results/EXP20220823_1/epoch_130.pth "
           "8"
 )
 
