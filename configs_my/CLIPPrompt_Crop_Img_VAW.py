b8ecc6f2c9da4293370619f92c9f5b5bdb8857ef
KyanChen <keyan1055002755@163.com>
2022-08-24 14:41:34 +0800

    add

--- configs_my/CLIPPrompt_Crop_Img_VAW.py
+++ configs_my/CLIPPrompt_Crop_Img_VAW.py
@@ -29,14 +29,14 @@ auto_scale_lr = dict(enable=False, base_batch_size=16)
 # data_root = 'D:/Dataset'
 data_root = '/data/kyanchen/prompt/data'
 
-# attribute_index_file = dict(
-#     file=data_root+'/VAW/common2common_att2id.json',
-#     att_group='common1'
-# )
 attribute_index_file = dict(
-    file=data_root+'/VAW/common2rare_att2id.json',
-    att_group='common'
+    file=data_root+'/VAW/common2common_att2id.json',
+    att_group='common1'
 )
+# attribute_index_file = dict(
+#     file=data_root+'/VAW/common2rare_att2id.json',
+#     att_group='common'
+# )
 model = dict(
     type='CLIP_Prompter',
     # classname_path=data_root+'/VAW/attribute_index.json',
