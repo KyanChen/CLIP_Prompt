b7df6d8fe2152afafe09f31dd1ffad5abbed7043
KyanChen <keyan1055002755@163.com>
2022-08-31 15:57:51 +0800

    add

--- tools/test_script.py
+++ tools/test_script.py
@@ -1,15 +1,15 @@
 import os
 import time
 
-
-os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
-          "sh dist_test.sh "
-          # "../configs_my/RPN_CLIPPrompt_Region_KD_COCO_VAW.py "
-          # '../configs_my/rpn_r50_fpn_mstrain_vg.py '
-          # '../configs_my/rpn_r50_fpn_mstrain_vaw.py '
-          # "../configs_my/rpn_r50_fpn_mstrain_coco.py "
-          "../configs_my/CLIPPrompt_Crop_Img_VAW.py "
-          f"results/EXP20220831_0/epoch_20.pth "
-          "8"
-)
+for i in range(20, 100, 20):
+    os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
+              "sh dist_test.sh "
+              # "../configs_my/RPN_CLIPPrompt_Region_KD_COCO_VAW.py "
+              # '../configs_my/rpn_r50_fpn_mstrain_vg.py '
+              # '../configs_my/rpn_r50_fpn_mstrain_vaw.py '
+              # "../configs_my/rpn_r50_fpn_mstrain_coco.py "
+              "../configs_my/CLIPPrompt_Crop_Img_VAW.py "
+              f"results/EXP20220831_0/epoch_{i}.pth "
+              "8"
+    )
 
