8251f271f550449d9d99e20b68e3acc4438f9077
KyanChen <keyan1055002755@163.com>
2022-08-31 20:28:28 +0800

    add

--- tools/test_script.py
+++ tools/test_script.py
@@ -9,7 +9,7 @@ for i in range(20, 100, 20):
               # '../configs_my/rpn_r50_fpn_mstrain_vaw.py '
               # "../configs_my/rpn_r50_fpn_mstrain_coco.py "
               "../configs_my/CLIPPrompt_Crop_Img_VAW.py "
-              f"results/EXP20220831_0/epoch_{i}.pth "
+              f"results/EXP20220831_9/epoch_{i}.pth "
               "8"
     )
 
