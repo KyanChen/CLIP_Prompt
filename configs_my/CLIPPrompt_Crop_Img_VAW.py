a0bec2b1450fdd181ebfef4c9b83a4f2fee2f791
KyanChen <keyan1055002755@163.com>
2022-08-31 16:12:48 +0800

    add

--- configs_my/CLIPPrompt_Crop_Img_VAW.py
+++ configs_my/CLIPPrompt_Crop_Img_VAW.py
@@ -69,10 +69,10 @@ model = dict(
     prompt_learner=dict(
         type='PromptAttributes',
         prompt_config=dict(
-            n_prompt=16,
+            n_prompt=30,
             is_att_specific=False,
-            att_position='end',
-            with_att_type=False,
+            att_position='mid',
+            with_att_type=True,
             context_length=77,
             n_prompt_type=None,
             generated_context=False,
@@ -161,7 +161,7 @@ data = dict(
         # ),
         attribute_index_file=dict(
             file=data_root+'/VAW/common2rare_att2id.json',
-            att_group='rare'
+            att_group='common'
         ),
         test_mode=True,
         open_category=False,
