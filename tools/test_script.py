1efc67b02086a04cc053948167a0316bfc419cbf
KyanChen <keyan1055002755@163.com>
2022-09-01 14:39:27 +0800

    add

--- tools/test_script.py
+++ tools/test_script.py
@@ -1,7 +1,7 @@
 import os
 import time
 
-for i in range(20, 100, 20):
+for i in range(20, 150, 20):
     os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
               "sh dist_test.sh "
               # "../configs_my/RPN_CLIPPrompt_Region_KD_COCO_VAW.py "
