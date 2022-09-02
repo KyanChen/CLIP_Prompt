bbf65c4f31311c6d08135885c7b87ef6b619f25e
KyanChen <keyan1055002755@163.com>
2022-09-02 15:31:58 +0800

    add

--- mmdet/models/backbones_my/clip/model.py
+++ mmdet/models/backbones_my/clip/model.py
@@ -58,8 +58,6 @@ class Bottleneck(nn.Module):
 class AttentionPool2d(nn.Module):
     def __init__(self, spacial_dim: int, embed_dim: int, num_heads: int, output_dim: int = None):
         super().__init__()
-        import pdb
-        pdb.set_trace()
         self.positional_embedding = nn.Parameter(torch.randn(spacial_dim ** 2 + 1, embed_dim) / embed_dim ** 0.5)
         self.k_proj = nn.Linear(embed_dim, embed_dim)
         self.q_proj = nn.Linear(embed_dim, embed_dim)
@@ -68,6 +66,8 @@ class AttentionPool2d(nn.Module):
         self.num_heads = num_heads
 
     def forward(self, x):
+        import pdb
+        pdb.set_trace()
         H = x.size(2)
         x = x.reshape(x.shape[0], x.shape[1], x.shape[2] * x.shape[3]).permute(2, 0, 1)  # NCHW -> (HW)NC
         x = torch.cat([x.mean(dim=0, keepdim=True), x], dim=0)  # (HW+1)NC
@@ -458,8 +458,6 @@ def build_model(state_dict: dict, with_attn=True, out_indices=(1, 2, 3, 4)):
     transformer_width = state_dict["ln_final.weight"].shape[0]
     transformer_heads = transformer_width // 64
     transformer_layers = len(set(k.split(".")[2] for k in state_dict if k.startswith(f"transformer.resblocks")))
-    import pdb
-    pdb.set_trace()
     model = CLIP(
         embed_dim,
         image_resolution, vision_layers, vision_width, vision_patch_size,
