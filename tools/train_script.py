339f81958603d45af5750bf0cfff7f7d1fe1d90a
KyanChen <keyan1055002755@163.com>
2022-09-02 23:24:56 +0800

    add

--- tools/train_script.py
+++ tools/train_script.py
@@ -12,11 +12,11 @@ while True:
     #           "../configs_my/CLIPPrompt_Crop_Img_VAW1.py "
     #           "results/EXP20220903_1 "
     #           "8")
-    os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
-              "sh dist_train.sh "
-              "../configs_my/CLIPPrompt_Crop_Img_VAW2.py "
-              "results/EXP20220903_2 "
-              "8")
+    # os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
+    #           "sh dist_train.sh "
+    #           "../configs_my/CLIPPrompt_Crop_Img_VAW2.py "
+    #           "results/EXP20220903_2 "
+    #           "8")
     os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
               "sh dist_train.sh "
               "../configs_my/CLIPPrompt_Crop_Img_VAW3.py "
