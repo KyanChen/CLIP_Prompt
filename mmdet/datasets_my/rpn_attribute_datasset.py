e70dc21f928574b83c71f867304df33ebeffae71
KyanChen <keyan1055002755@163.com>
2022-09-05 16:55:45 +0800

    add

--- mmdet/datasets_my/rpn_attribute_datasset.py
+++ mmdet/datasets_my/rpn_attribute_datasset.py
@@ -54,8 +54,6 @@ class RPNAttributeDataset(Dataset):
             self.kd_pipeline = kd_pipeline
 
         self.data_root = data_root
-        import pdb
-        pdb.set_trace()
         if test_mode:
             id2images_vaw, id2instances_vaw = self.read_data_vaw(dataset_split)
             self.id2images = id2images_vaw
