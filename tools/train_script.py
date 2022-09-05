f05c749b084c9665e6f87b101074fa89b1d5ae4b
KyanChen <keyan1055002755@163.com>
2022-09-05 13:43:49 +0800

    add

--- tools/train_script.py
+++ tools/train_script.py
@@ -4,8 +4,8 @@ import time
 while True:
     os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
               "sh dist_train.sh "
-              "../configs_my/CLIPPrompt_Crop_Img_VAW.py "
-              "results/EXP20220903_7 "
+              "../configs_my/RPN_CLIPPrompt_Region_KD_COCO_VAW.py "
+              "results/EXP20220905_0 "
               "8")
     # os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
     #           "sh dist_train.sh "
