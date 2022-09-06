e11b685f5edcf4ed5876535cf2c183927465d6ac
KyanChen <keyan1055002755@163.com>
2022-09-06 16:08:15 +0800

    add

--- configs_my/CLIPPrompt_Crop_Img_VAW.py
+++ configs_my/CLIPPrompt_Crop_Img_VAW.py
@@ -216,17 +216,16 @@ optimizer = dict(
 
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 
-# # learning policy
-lr_config = dict(
-    policy='step',
-    warmup='linear',
-    warmup_iters=2000,
-    warmup_ratio=0.1,
-    # gamma=0.5,
-    # step=[50, 80],
-    step=[30, 45]
-)
-
+# lr_config = dict(
+#     policy='step',
+#     warmup='linear',
+#     warmup_iters=2000,
+#     warmup_ratio=0.1,
+#     # gamma=0.5,
+#     # step=[50, 80],
+#     step=[30, 45]
+# )
+lr_config = None
 # lr_config = dict(
 #     policy='CosineAnnealing',
 #     by_epoch=False,
