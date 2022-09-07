96b9d2bbe5b3609a06399eda7042042ab5b287a7
KyanChen <keyan1055002755@163.com>
2022-09-07 10:02:01 +0800

    add

--- tools/test_loop.py
+++ tools/test_loop.py
@@ -2,7 +2,8 @@ import os
 import time
 
 for i_epoch in range(10, 60, 10):
-    os.system(f"python test.py "
+    os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
+              "sh dist_test.sh "
               f"--config ../configs_my/RPN_CLIPPrompt_Region_KD_COCO_VAW.py "
               f"--checkpoint results/EXP20220905_0/epoch_{i_epoch}.pth"
               )
