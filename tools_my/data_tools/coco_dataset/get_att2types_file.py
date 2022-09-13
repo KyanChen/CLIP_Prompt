14d2d3f757073ea134fb6d495737673c58242a84
KyanChen <keyan1055002755@163.com>
2022-09-13 17:03:16 +0800

    add

--- tools_my/data_tools/coco_dataset/get_att2types_file.py
+++ tools_my/data_tools/coco_dataset/get_att2types_file.py
@@ -5,13 +5,13 @@ file = '/Users/kyanchen/My_Code/prompt/objects/MSCOCO/categories.json'
 save_path = '../../../attributes/COCO'
 
 type_data = json.load(open(file, 'r'))
-att2types = {'id2type': {}, 'category2typeid': {}}
+att2types = {'id2type': {}, 'att2typeid': {}}
 types = list(set([x["supercategory"] for x in type_data]))
 for idx in range(len(types)):
     att2types['id2type'][idx] = types[idx]
 for item in type_data:
-    att2types['category2typeid'][item['name']] = types.index(item['supercategory'])
-att2types['num_category'] = len(att2types['category2typeid'])
+    att2types['att2typeid'][item['name']] = types.index(item['supercategory'])
+att2types['num_category'] = len(att2types['att2typeid'])
 att2types['num_type'] = len(att2types['id2type'])
 json.dump(att2types, open(save_path+'/category2types.json', 'w'), indent=4)
 
