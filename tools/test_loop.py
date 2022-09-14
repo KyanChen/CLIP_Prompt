b8c114cf7efa0bf0aefaa3192bff8daa0a7ec1af
KyanChen <keyan1055002755@163.com>
2022-09-14 11:19:18 +0800

    add

--- tools/test_loop.py
+++ tools/test_loop.py
@@ -1,7 +1,7 @@
 import os
 import time
 
-for i_epoch in range(5, 55, 5):
+for i_epoch in range(15, 55, 5):
     os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
               "sh dist_test.sh "
               f"../configs_my/CLIPPrompt_Crop_Img_VAW.py "
