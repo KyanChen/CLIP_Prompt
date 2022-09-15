3fcecfc854a07fb97a24ae4d52da76536ffa42bc
KyanChen <keyan1055002755@163.com>
2022-09-15 12:15:19 +0800

    add

--- configs_my/CLIPPrompt_Crop_Img_VAW.py
+++ configs_my/CLIPPrompt_Crop_Img_VAW.py
@@ -35,10 +35,10 @@ data_root = '/data/kyanchen/prompt/data'
 # )
 
 attribute_index_file = dict(
-    att_file='../attributes/VAW/common2common_att2id.json',
-    att_group='common2',
-    # att_file='../attributes/OVAD/common2common_att2id.json',
-    # att_group='common1',
+    # att_file='../attributes/VAW/common2common_att2id.json',
+    # att_group='common2',
+    att_file='../attributes/OVAD/common2common_att2id.json',
+    att_group='common1',
     # category_file='../attributes/COCO/common2common_category2id.json',
     # category_group='common1',
 )
@@ -81,8 +81,8 @@ model = dict(
             n_prompt=30,
             is_att_specific=False,
             att_position='mid',
-            att2type='../attributes/VAW/att2types.json',
-            # att2type='../attributes/OVAD/att2types.json',
+            # att2type='../attributes/VAW/att2types.json',
+            att2type='../attributes/OVAD/att2types.json',
             context_length=77,
             n_prompt_type=None,
             generated_context=False,
@@ -220,10 +220,10 @@ data = dict(
         data_root=data_root,
         dataset_split='test',
         attribute_index_file=dict(
-            att_file='../attributes/VAW/common2common_att2id.json',
-            att_group='common2',
-            # att_file='../attributes/OVAD/common2common_att2id.json',
-            # att_group='common1',
+            # att_file='../attributes/VAW/common2common_att2id.json',
+            # att_group='common2',
+            att_file='../attributes/OVAD/common2common_att2id.json',
+            att_group='common1',
         ),
         # attribute_index_file=dict(
         #     file=data_root+'/VAW/common2rare_att2id.json',
@@ -231,7 +231,7 @@ data = dict(
         # ),
         test_mode=True,
         open_category=False,
-        dataset_names='vaw',
+        dataset_names='ovad',
         save_label=False,
         load_label=None,
         pipeline=test_pipeline
