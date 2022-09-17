5d7df57c0cbcf7e5528a458d590d518c0f9b2a36
KyanChen <keyan1055002755@163.com>
2022-09-17 11:06:48 +0800

    add

--- configs_my/CLIPPrompt_Crop_Img_VAW.py
+++ configs_my/CLIPPrompt_Crop_Img_VAW.py
@@ -35,12 +35,12 @@ data_root = '/data/kyanchen/prompt/data'
 # )
 
 attribute_index_file = dict(
-    att_file='../attributes/VAW/common2common_att2id.json',
-    att_group='common2',
-    # att_file='../attributes/OVAD/common2common_att2id.json',
-    # att_group='common1',
+    # att_file='../attributes/VAW/common2common_att2id.json',
+    # att_group='common2',
+    att_file='../attributes/OVAD/common2common_att2id.json',
+    att_group='common1',
     category_file='../attributes/COCO/common2common_category2id_48_17.json',
-    category_group='common2',
+    category_group='common1',
 )
 
 # attribute_index_file = dict(
@@ -81,8 +81,8 @@ model = dict(
             n_prompt=30,
             is_att_specific=False,
             att_position='mid',
-            att2type='../attributes/VAW/att2types.json',
-            # att2type='../attributes/OVAD/att2types.json',
+            # att2type='../attributes/VAW/att2types.json',
+            att2type='../attributes/OVAD/att2types.json',
             context_length=77,
             n_prompt_type=None,
             generated_context=False,
@@ -105,15 +105,15 @@ model = dict(
     neck=None,
     bbox_head=dict(
         type='PromptHead',
-        attr_freq_file='../attributes/VAW/attr_freq_wo_sort.json',
+        # attr_freq_file='../attributes/VAW/attr_freq_wo_sort.json',
         category_freq_file='../attributes/COCO/category_freq_wo_sort.json',
         re_weight_different_att=0.25,
         re_weight_category=2,
         re_weight_gamma=2,
         re_weight_beta=0.995,
         # balance_unk=0.2,  # finetune
-        balance_unk=0.15,
-        # balance_unk=1  # gen
+        # balance_unk=0.15,
+        balance_unk=1  # gen
     )
 )
 img_scale = (224, 224)  # (224, 224) (288, 288) (336, 336), (384, 384) (448, 448)
@@ -197,7 +197,7 @@ data = dict(
         #     file=data_root+'/VAW/common2rare_att2id.json',
         #     att_group='rare'
         # ),
-        dataset_names=['coco', 'vaw'],
+        dataset_names=['coco', 'ovadgen'],
         save_label=False,
         load_label=None,
         test_mode=False,
@@ -211,7 +211,7 @@ data = dict(
         data_root=data_root,
         dataset_split='test',
         attribute_index_file=attribute_index_file,
-        dataset_names=['coco', 'vaw'],
+        dataset_names=['coco', 'ovad'],
         test_mode=True,
         open_category=False,
         pipeline=test_pipeline),
