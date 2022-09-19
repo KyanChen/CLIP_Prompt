c66f03b7fa0493755cc5635aa252430304319ceb
KyanChen <keyan1055002755@163.com>
2022-09-19 15:23:38 +0800

    add

--- configs_my/CLIPPrompt_Crop_Img_VAW.py
+++ configs_my/CLIPPrompt_Crop_Img_VAW.py
@@ -226,7 +226,7 @@ data = dict(
             att_file='../attributes/OVAD/common2common_att2id.json',
             att_group='common1',
             category_file='../attributes/COCO/common2common_category2id_48_17.json',
-            category_group='common1',
+            category_group='common2',
         ),
         test_mode=True,
         open_category=False,
