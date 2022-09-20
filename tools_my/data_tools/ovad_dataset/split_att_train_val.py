da56e5c47e078a519d50ab1800328b59e7c141ea
KyanChen <keyan1055002755@163.com>
2022-09-20 22:07:14 +0800

    add

--- tools_my/data_tools/ovad_dataset/split_att_train_val.py
+++ tools_my/data_tools/ovad_dataset/split_att_train_val.py
@@ -10,10 +10,19 @@ for idx, item in enumerate(data):
         common2common['common1'].update({item['name']: idx})
     elif item["ov_set"] == "novel":
         common2common['common2'].update({item['name']: idx})
+
+idx = 0
+for k, v in common2common['common1'].items():
+    common2common['common1'][k] = idx
+    idx += 1
+for k, v in common2common['common2'].items():
+    common2common['common2'][k] = idx
+    idx += 1
+
 common2common['common1_len'] = len(common2common['common1'])
 common2common['common2_len'] = len(common2common['common2'])
 
-json.dump(common2common, open(save_path+'/common2common_category2id_48_17.json', 'w'), indent=4)
+json.dump(common2common, open(save_path+'/common2common_category2id_48_32.json', 'w'), indent=4)
 
 
 data = json.load(open(file_path))['attributes']
