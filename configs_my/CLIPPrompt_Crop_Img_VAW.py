8eecf4d146ffd57b1820665aea8bee272eaa6406
KyanChen <keyan1055002755@163.com>
2022-09-20 22:10:03 +0800

    add

--- configs_my/CLIPPrompt_Crop_Img_VAW.py
+++ configs_my/CLIPPrompt_Crop_Img_VAW.py
@@ -40,7 +40,7 @@ attribute_index_file = dict(
     att_file='../attributes/OVAD/common2common_att2id.json',
     att_group='common1',
     # category_file='../attributes/COCO/common2common_category2id_48_17.json',
-    category_file='../attributes/OVAD/common2common_category2id_48_32.json',
+    category_file='../attributes/COCO/common2common_category2id_48_32.json',
     category_group='common1',
 )
 
@@ -228,7 +228,7 @@ data = dict(
             att_file='../attributes/OVAD/common2common_att2id.json',
             att_group='common1',
             # category_file='../attributes/COCO/common2common_category2id_48_17.json',
-            category_file='../attributes/OVAD/common2common_category2id_48_32.json',
+            category_file='../attributes/COCO/common2common_category2id_48_32.json',
             category_group='common1',
         ),
         test_mode=True,
