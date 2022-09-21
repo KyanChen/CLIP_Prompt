6e44dd73f704f7707c275c81a8c303170ae4f73a
KyanChen <keyan1055002755@163.com>
2022-09-21 16:27:40 +0800

    add

--- configs_my/CLIPPrompt_Crop_Img_VAW.py
+++ configs_my/CLIPPrompt_Crop_Img_VAW.py
@@ -35,13 +35,13 @@ data_root = '/data/kyanchen/prompt/data'
 # )
 
 attribute_index_file = dict(
-    # att_file='../attributes/VAW/common2common_att2id.json',
-    # att_group='common2',
-    att_file='../attributes/OVAD/common2common_att2id.json',
+    att_file='../attributes/VAW/common2common_att2id.json',
     att_group='common1',
-    category_file='../attributes/COCO/common2common_category2id_48_17.json',
-    # category_file='../attributes/COCO/common2common_category2id_48_32.json',
-    category_group='common2',
+    # att_file='../attributes/OVAD/common2common_att2id.json',
+    # att_group='common1',
+    # category_file='../attributes/COCO/common2common_category2id_48_17.json',
+    # # category_file='../attributes/COCO/common2common_category2id_48_32.json',
+    # category_group='common2',
 )
 
 # attribute_index_file = dict(
@@ -53,7 +53,7 @@ model = dict(
     # classname_path=data_root+'/VAW/attribute_index.json',
     attribute_index_file=attribute_index_file,
     need_train_names=[
-        'prompt_category_learner',
+        # 'prompt_category_learner',
         'prompt_att_learner',
         # 'image_encoder',
         'text_encoder',
@@ -80,30 +80,30 @@ model = dict(
     prompt_att_learner=dict(
         type='PromptAttributes',
         prompt_config=dict(
-            n_prompt=0,
+            n_prompt=30,
             is_att_specific=False,
             att_position='mid',
-            # att2type='../attributes/VAW/att2types.json',
-            att2type='../attributes/OVAD/att2types.json',
+            att2type='../attributes/VAW/att2types.json',
+            # att2type='../attributes/OVAD/att2types.json',
             context_length=77,
             n_prompt_type=None,
             generated_context=False,
             pos_emb=False,
         ),
     ),
-    prompt_category_learner=dict(
-        type='PromptAttributes',
-        prompt_config=dict(
-            n_prompt=0,
-            is_att_specific=False,
-            att_position='mid',
-            att2type='../attributes/COCO/category2types.json',
-            context_length=77,
-            n_prompt_type=None,
-            generated_context=False,
-            pos_emb=False,
-        ),
-    ),
+    # prompt_category_learner=dict(
+    #     type='PromptAttributes',
+    #     prompt_config=dict(
+    #         n_prompt=0,
+    #         is_att_specific=False,
+    #         att_position='mid',
+    #         att2type='../attributes/COCO/category2types.json',
+    #         context_length=77,
+    #         n_prompt_type=None,
+    #         generated_context=False,
+    #         pos_emb=False,
+    #     ),
+    # ),
     neck=None,
     bbox_head=dict(
         type='PromptHead',
@@ -223,17 +223,17 @@ data = dict(
         data_root=data_root,
         dataset_split='test',
         attribute_index_file=dict(
-            # att_file='../attributes/VAW/common2common_att2id.json',
-            # att_group='common2',
-            att_file='../attributes/OVAD/common2common_att2id.json',
+            att_file='../attributes/VAW/common2common_att2id.json',
             att_group='common1',
-            category_file='../attributes/COCO/common2common_category2id_48_17.json',
-            # # category_file='../attributes/COCO/common2common_category2id_48_32.json',
-            category_group='common2',
+            # att_file='../attributes/OVAD/common2common_att2id.json',
+            # att_group='common1',
+            # category_file='../attributes/COCO/common2common_category2id_48_17.json',
+            # # # category_file='../attributes/COCO/common2common_category2id_48_32.json',
+            # category_group='common2',
         ),
         test_mode=True,
         open_category=False,
-        dataset_names=['ovadattr', 'coco'],
+        dataset_names=['vaw'],
         save_label=False,
         load_label=None,
         pipeline=test_pipeline
