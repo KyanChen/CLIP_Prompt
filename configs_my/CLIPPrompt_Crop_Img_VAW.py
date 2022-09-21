64df697ac1e7f58413d81820fecdfc35a8a75c99
KyanChen <keyan1055002755@163.com>
2022-09-21 16:06:52 +0800

    add

--- configs_my/CLIPPrompt_Crop_Img_VAW.py
+++ configs_my/CLIPPrompt_Crop_Img_VAW.py
@@ -36,12 +36,12 @@ data_root = '/data/kyanchen/prompt/data'
 
 attribute_index_file = dict(
     att_file='../attributes/VAW/common2common_att2id.json',
-    att_group='common1',
+    att_group='common2',
     # att_file='../attributes/OVAD/common2common_att2id.json',
     # att_group='common1',
     category_file='../attributes/COCO/common2common_category2id_48_17.json',
     # category_file='../attributes/COCO/common2common_category2id_48_32.json',
-    category_group='common1',
+    category_group='common2',
 )
 
 # attribute_index_file = dict(
@@ -224,12 +224,12 @@ data = dict(
         dataset_split='test',
         attribute_index_file=dict(
             att_file='../attributes/VAW/common2common_att2id.json',
-            att_group='common1',
+            att_group='common2',
             # att_file='../attributes/OVAD/common2common_att2id.json',
             # att_group='common1',
             category_file='../attributes/COCO/common2common_category2id_48_17.json',
             # category_file='../attributes/COCO/common2common_category2id_48_32.json',
-            category_group='common1',
+            category_group='common2',
         ),
         test_mode=True,
         open_category=False,
