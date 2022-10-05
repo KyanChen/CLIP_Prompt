cd501e4fed5adf57a44d95be50bacb5cfeb67356
KyanChen <keyan1055002755@163.com>
2022-10-05 21:38:48 +0800

    add

--- mmdet/models/detectors_my/__init__.py
+++ mmdet/models/detectors_my/__init__.py
@@ -4,7 +4,11 @@ from .maskrcnn_clip import MaskRCNNCLIP
 from .faster_rcnn_rpn import FasterRCNNRPN
 from .clip_prompt_region import CLIP_Prompter_Region
 from .rpn_clip_prompt_region import RPN_CLIP_Prompter_Region
+from .clip_tester import CLIP_Tester
 
 __all__ =[
-    'CLIP_Prompter', 'OFA_Prompter', 'MaskRCNNCLIP', 'FasterRCNNRPN', 'CLIP_Prompter_Region', 'RPN_CLIP_Prompter_Region'
+    'CLIP_Prompter', 'OFA_Prompter',
+    'MaskRCNNCLIP', 'FasterRCNNRPN',
+    'CLIP_Prompter_Region', 'RPN_CLIP_Prompter_Region',
+    'CLIP_Tester'
 ]
