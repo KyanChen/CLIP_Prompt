ea81775429a6dec9f53aa9271074c7551b389b35
KyanChen <keyan1055002755@163.com>
2022-10-06 17:16:42 +0800

    add

--- configs_my/CLIPPrompt_Crop_Img_VAW.py
+++ configs_my/CLIPPrompt_Crop_Img_VAW.py
@@ -255,7 +255,7 @@ optimizer = dict(
     constructor='SubModelConstructor',
     sub_model={
         'prompt_att_learner': {},
-        # 'prompt_category_learner': {},
+        'prompt_category_learner': {},
         # 'image_encoder': {'lr_mult': 0.1},
         'text_encoder': {'lr_mult': 0.1},
         'bbox_head': {}, 'logit_scale': {}
