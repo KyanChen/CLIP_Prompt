0eb0a2046c42628f71e835b9ef61a3f8ed1cc86a
KyanChen <keyan1055002755@163.com>
2022-10-06 17:15:44 +0800

    add

--- configs_my/CLIPPrompt_Crop_Img_VAW.py
+++ configs_my/CLIPPrompt_Crop_Img_VAW.py
@@ -35,15 +35,18 @@ data_root = '/data/kyanchen/prompt/data'
 # )
 
 attribute_index_file = dict(
-    # att_file='../attributes/VAW/common2common_att2id.json',
-    # att_group='common1',
+    att_file='../attributes/VAW/common2common_att2id.json',
+    att_group='common1',
     # att_file='../attributes/VAW/common2rare_att2id.json',
     # att_group='common+rare',
     # att_file='../attributes/OVAD/common2common_att2id.json',
     # att_group='common1',
+    # category_file='../attributes/COCO/common2common_category2id_48_17.json',
+    # # category_file='../attributes/COCO/common2common_category2id_48_32.json',
+    # category_group='common1+common2',
     category_file='../attributes/COCO/common2common_category2id_48_17.json',
     # category_file='../attributes/COCO/common2common_category2id_48_32.json',
-    category_group='common1+common2',
+    category_group='common1',
 )
 
 # attribute_index_file = dict(
@@ -55,7 +58,7 @@ model = dict(
     # classname_path=data_root+'/VAW/attribute_index.json',
     attribute_index_file=attribute_index_file,
     need_train_names=[
-        # 'prompt_category_learner',
+        'prompt_category_learner',
         'prompt_att_learner',
         # 'image_encoder',
         'text_encoder',
@@ -79,21 +82,21 @@ model = dict(
     #     class_token_position='end'
     # ),
     shared_prompt_vectors=False,
-    # prompt_att_learner=dict(
-    #     type='PromptAttributes',
-    #     prompt_config=dict(
-    #         n_prompt=30,
-    #         is_att_specific=False,
-    #         att_position='mid',
-    #         att2type='../attributes/VAW/att2types.json',
-    #         # att2type=None,
-    #         # att2type='../attributes/OVAD/att2types.json',
-    #         context_length=77,
-    #         n_prompt_type=None,
-    #         generated_context=False,
-    #         pos_emb=False,
-    #     ),
-    # ),
+    prompt_att_learner=dict(
+        type='PromptAttributes',
+        prompt_config=dict(
+            n_prompt=30,
+            is_att_specific=False,
+            att_position='mid',
+            att2type='../attributes/VAW/att2types.json',
+            # att2type=None,
+            # att2type='../attributes/OVAD/att2types.json',
+            context_length=77,
+            n_prompt_type=None,
+            generated_context=False,
+            pos_emb=False,
+        ),
+    ),
     prompt_category_learner=dict(
         type='PromptAttributes',
         prompt_config=dict(
@@ -111,8 +114,8 @@ model = dict(
     neck=None,
     bbox_head=dict(
         type='PromptHead',
-        # attr_freq_file='../attributes/VAW/attr_freq_wo_sort.json',
-        # category_freq_file='../attributes/COCO/category_freq_wo_sort.json',
+        attr_freq_file='../attributes/VAW/attr_freq_wo_sort.json',
+        category_freq_file='../attributes/COCO/category_freq_wo_sort.json',
         re_weight_different_att=0.25,
         re_weight_category=2,
         re_weight_gamma=2,
