476e0c9ebd432cd06398f2721baf696d4e21c61f
KyanChen <keyan1055002755@163.com>
2022-10-07 13:55:45 +0800

    add

--- mmdet/models/detectors_my/clip_prompt.py
+++ mmdet/models/detectors_my/clip_prompt.py
@@ -269,7 +269,8 @@ class CLIP_Prompter(BaseDetector):
             text_features_cate = self.text_encoder(prompt_context, eot_index)
             text_features.append(text_features_cate)
         text_features = torch.cat(text_features, dim=0)
-
+        import pdb
+        pdb.set_trace()
         # prompt_context = self.prompt_learner()  # 620x77x512
         # text_features = self.text_encoder(prompt_context, self.tokenized_prompts)
 
