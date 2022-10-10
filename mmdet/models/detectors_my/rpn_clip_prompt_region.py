4b512136cb683c45b509599a235cc88f39a13a65
KyanChen <keyan1055002755@163.com>
2022-10-10 22:14:30 +0800

    add

--- mmdet/models/detectors_my/rpn_clip_prompt_region.py
+++ mmdet/models/detectors_my/rpn_clip_prompt_region.py
@@ -346,7 +346,8 @@ class RPN_CLIP_Prompter_Region(BaseModule):
         patch_dataset_type = [dataset_type[idx] for idx, x in enumerate(gt_bboxes) for _ in range(len(x))]
         patch_dataset_type = torch.tensor(patch_dataset_type).to(img.device)
 
-        patch_gt_label = [i for x in enumerate(gt_labels) for i in x]
+        patch_gt_label = [label for x_per_img in enumerate(gt_labels) for label in x_per_img]
+
         patch_gt_label = torch.stack(patch_gt_label, dim=0)
         boxes_feats, bbox_feat_maps = self.att_head(img_f_maps, gt_bboxes)
         text_features = []
