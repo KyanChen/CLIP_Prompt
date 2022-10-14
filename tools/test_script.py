e3909af06798755bb69fcb3317604ca76ef4a604
KyanChen <keyan1055002755@163.com>
2022-10-14 21:39:14 +0800

    add

--- tools/test_script.py
+++ tools/test_script.py
@@ -1,7 +1,7 @@
 import os
 import time
 
-os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
+os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 "
               "sh dist_test.sh "
               # "../configs_my/RPN_CLIPPrompt_Region_KD_COCO_VAW.py "
               # '../configs_my/rpn_r50_fpn_mstrain_vg.py '
