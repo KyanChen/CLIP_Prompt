70dc94df6daee6d0126a77f099646d167c4669be
KyanChen <keyan1055002755@163.com>
2022-10-14 14:54:38 +0800

    add

--- tools/test_loop.py
+++ tools/test_loop.py
@@ -5,7 +5,7 @@ for i_epoch in range(2, 40, 2):
     os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
               "sh dist_test.sh "
               f"../configs_my/Op1_CLIPPrompt_Crop_Img_VAW.py "
-              f"results/EXP20221013_0/epoch_{i_epoch}.pth "
+              f"results/EXP20221013_1/epoch_{i_epoch}.pth "
               # f"results/EXP20220916_/epoch_{i_epoch}.pth "
               "8"
               )
