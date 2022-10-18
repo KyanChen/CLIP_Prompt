d9b1231064729cf75df9c3a50693bcce1abc0f7e
KyanChen <keyan1055002755@163.com>
2022-10-18 22:26:27 +0800

    add

--- tools/test_loop.py
+++ tools/test_loop.py
@@ -6,7 +6,7 @@ for i_epoch in range(5, 65, 5):
               "sh dist_test.sh "
               # f"../configs_my/Op1_CLIPPrompt_Crop_Img_VAW.py "
               f"../configs_my/Op2_RPN_CLIPPrompt_Region_KD_COCO_VAW.py "
-              f"results/EXP20221016_0/epoch_{i_epoch}.pth "
+              f"results/EXP20221016_1/epoch_{i_epoch}.pth "
               # f"results/EXP20220916_/epoch_{i_epoch}.pth "
               "8"
               )
