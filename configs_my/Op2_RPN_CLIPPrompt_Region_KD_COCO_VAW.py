649214cb213d88d843d3c0e58306b5c773b15781
KyanChen <keyan1055002755@163.com>
2022-10-19 23:50:07 +0800

    add

--- configs_my/Op2_RPN_CLIPPrompt_Region_KD_COCO_VAW.py
+++ configs_my/Op2_RPN_CLIPPrompt_Region_KD_COCO_VAW.py
@@ -125,7 +125,7 @@ model = dict(
             use_abs_pos_embed=True,
             drop_rate=0.1,
             class_token=True,
-            num_encoder_layers=5,
+            num_encoder_layers=4,
             global_pool=False,
         )
     ),
