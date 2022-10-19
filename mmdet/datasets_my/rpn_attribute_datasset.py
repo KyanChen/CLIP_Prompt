ec05c718a5861899b25d44a6d2b3794cfecb55be
KyanChen <keyan1055002755@163.com>
2022-10-19 12:50:03 +0800

    add

--- mmdet/datasets_my/rpn_attribute_datasset.py
+++ mmdet/datasets_my/rpn_attribute_datasset.py
@@ -1452,7 +1452,7 @@ class RPNAttributeDataset(Dataset):
         coco_gt = COCO()
         # coco_gt.dataset['images'] = [img for img in self.dataset['images']]
 
-        coco_gt_cates = {k: {'id': v} for k, v in self.category2id.items()}
+        coco_gt_cates = [{'id': v, "name": k, 'supercategory': 'none'} for k, v in self.category2id.items()]
         coco_gt.dataset['categories'] = coco_gt_cates
 
 
