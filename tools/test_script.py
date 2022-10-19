5aca395c8be8c75b06ff9b522e9726dae0c42f9a
KyanChen <keyan1055002755@163.com>
2022-10-19 16:55:34 +0800

    add

--- tools/test_script.py
+++ tools/test_script.py
@@ -9,8 +9,8 @@ os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5 "
               # "../configs_my/rpn_r50_fpn_mstrain_coco.py "
               # "../configs_my/Op3_RPN_coco.py "
               # f"../pretrain/faster_rcnn_x101_64x4d_fpn_mstrain_3x_coco_20210524_124528-26c63de6.pth "
-              "../configs_my/Op2_RPN_CLIPPrompt_Region_KD_COCO_VAW.py "
-              f"results/EXP20221010_0/epoch_60.pth "
+              "../configs_my/Op1_CLIPPrompt_Crop_Img_VAW.py "
+              f"results/EXP20221006_0/epoch_60.pth "
               "8"
     )
 
