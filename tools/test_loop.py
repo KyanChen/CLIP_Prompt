358e2b7aaecec843c280c77f079e7f3d13c72ae5
KyanChen <keyan1055002755@163.com>
2022-10-22 10:29:37 +0800

    add

--- tools/test_loop.py
+++ tools/test_loop.py
@@ -1,12 +1,12 @@
 import os
 import time
 
-for i_epoch in range(4, 20, 4):
+for i_epoch in range(10, 60, 10):
     os.system("CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 "
               "sh dist_test.sh "
-              f"../configs_my/Op1_CLIPPrompt_Crop_Img_VAW.py "
-              # f"../configs_my/Op2_RPN_CLIPPrompt_Region_KD_COCO_VAW.py "
-              f"results/EXP20221013_0/epoch_{i_epoch}.pth "
+              # f"../configs_my/Op1_CLIPPrompt_Crop_Img_VAW.py "
+              f"../configs_my/Op2_RPN_CLIPPrompt_Region_KD_COCO_VAW.py "
+              f"results/EXP20221020_0/epoch_{i_epoch}.pth "
               # f"results/EXP20220916_/epoch_{i_epoch}.pth "
               "8"
               )
