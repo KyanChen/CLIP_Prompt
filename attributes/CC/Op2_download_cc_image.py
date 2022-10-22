f63911f9b51c76bb2d3c620e469364fac5d79905
KyanChen <keyan1055002755@163.com>
2022-10-22 15:54:31 +0800

    add

--- attributes/CC/Op2_download_cc_image.py
+++ attributes/CC/Op2_download_cc_image.py
@@ -1,10 +1,9 @@
 import json
 import os
 import argparse
-# import img2dataset
+import img2dataset
 import pandas as pd
 from tqdm import tqdm
-from concurrent import futures
 
 def parse_args():
     parser = argparse.ArgumentParser(
@@ -15,7 +14,7 @@ def parse_args():
                         help='index of the LVIS/COCO base categories')
     parser.add_argument('--base-category', action='store_true',
                         help='whether to retrieval the images of the base categories')
-    parser.add_argument('--output-folder', default='data/cc/images',
+    parser.add_argument('--output-folder', default='/Users/kyanchen/Documents/CC/images',
                         help='output path')
     parser.add_argument('--num-thread', type=int, default=10,
                         help='the number of the thread to download the images')
@@ -23,46 +22,24 @@ def parse_args():
     return args
 
 
-def download_fun(cls_names, output_folder):
-    for i, cls_name in tqdm(enumerate(cls_names), total=len(cls_names)):
-        file_path = os.path.join(output_folder, cls_name + ".txt")
-        image_path = os.path.join(output_folder, cls_name)
-        img2dataset.download(url_list=file_path, image_size=1024, output_folder=image_path, processes_count=64, timeout=20)
-
-    return True
-
 def open_tsv(fname):
     print("Opening %s Data File..." % fname)
     df = pd.read_csv(fname, sep='\t', header=None)
     print("Processing", len(df), " Images:")
     return df
 
+
 def main():
     args = parse_args()
-
     output_folder = args.output_folder
     name_file = args.name_file
-    base_ind_file = args.base_ind_file
-    num_thread = args.num_thread
-
     df = open_tsv(name_file)
     valid_idxs_dict = json.load(open(args.valid_ind_file))
-    base_inds = open(base_ind_file, 'r').readline().strip().split(', ')
-    base_inds = [int(ind) for ind in base_inds]
-    novel_inds = [i for i in range(len(names)) if i not in base_inds]
-
-    if args.base_category:
-        names = [names[i] for i in base_inds]
-    else:
-        names = [names[i] for i in novel_inds]
-
-    count_per_thread = (len(names) + num_thread - 1) // num_thread
-    names_list = [names[i * count_per_thread:(i + 1) * count_per_thread] for i in range(num_thread)]
-
-    with futures.ThreadPoolExecutor(max_workers=num_thread) as executor:
-        threads = [executor.submit(download_fun, name, output_folder=output_folder) for name in names_list]
-        for future in futures.as_completed(threads):
-            print(future.result())
+    url_list = [df.iloc[int(idx), 1] for idx in valid_idxs_dict.keys()]
+    url_list = [x+'\n' for idx, x in enumerate(url_list)]
+    tmp_file = open('tmp_url_list.txt', 'w')
+    tmp_file.writelines(url_list)
+    img2dataset.download(url_list='tmp_url_list.txt', image_size=1024, output_folder=output_folder, processes_count=1, timeout=20)
 
 
 if __name__ == '__main__':
